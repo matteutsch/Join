cadd7e6aa3eb1a740b95ab0b9a310da1f14db4bb
User42-1 <jens.schrecker@t-online.de>
2023-05-03 18:54:18 +0200

    added data.js (sample data) and greeting function according to daytime

--- javascripts/data.js
+++ javascripts/data.js
@@ -3,43 +3,55 @@ let contacts = [{
     'initials':'AM',
     'email':'antom@gmail.com',
 },{
-    'name':'Anton Mayer',
-    'initials':'AM',
-    'email':'antom@gmail.com',
+    'name':'Anja Schulz',
+    'initials':'AS',
+    'email':'schulz@hotmail',
+},{
+    'name':'Benedikt Ziegler',
+    'initials':'BZ',
+    'email':'benedikt@gmail',
+},{
+    'name':'David Eisenberg',
+    'initials':'DE',
+    'email':'davidberg@gmail',
 }]
+
+let state = ['To Do','In Progress','Awaiting Feedback','Done']
+let cat = ['Design','Sales','Backoffice','Marketing','Media']
+let prio = ['urgent','medium','low']
 
 let tasks = [{
     'titel':'',
     'description':'',
-    'status':['To Do','In Progress','Awaiting Feedback','Done'],
-    'category':['Design','Sales','Backoffice','Marketing','Media'],
+    'status': state[0],
+    'category':cat[1],
+    'priority':prio[2],
     'subtasks':[],
     'dueDate':'',
-    'priority':['urgent','medium','low'],
-    'assignedTo':[],
+    'assignedTo':[contacts[0],contacts[2],contacts[3]]
 },{
     'titel':'',
     'description':'',
-    'status':['To Do','In Progress','Awaiting Feedback','Done'],
-    'category':['Design','Sales','Backoffice','Marketing','Media'],
+    'status': state[2],
+    'category':cat[4],
+    'priority':prio[0],
     'subtasks':[],
     'dueDate':'',
-    'priority':['urgent','medium','low'],
-    'assignedTo':[],
+    'assignedTo':[contacts[2],contacts[3],contacts[4]]
 }]
 
 
-// Generate Greeting according to daytime
+// Generate Greeting according to daytime after 1.2 seconds
 setInterval(function () {
     let date = new Date();
     let hours = date.getHours();
     let greeting;
     if (hours < 12) {
-        greeting = 'Good Morning,';
+        greeting = 'Good morning,';
     } else if (hours >= 12 && hours < 17) {
         timeOfDay = 'Good afternoon,';
     } else {
         greeting = 'Good evening, ';
     }
     document.getElementById('greeting').innerHTML = timeOfDay, ',';
-}, 1000);
+}, 1200);
