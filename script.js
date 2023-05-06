1fe78e28d6c59aa05f9da9bb7b5db78165706db4
FurkanAyhan <109309647+FurkanAy97@users.noreply.github.com>
2023-05-06 19:24:09 +0200

    add responsive design signup + improved animation

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
+function initLoginPage() {
+  fadeInAnimation();
+  changeLogoColor();
+}
 
+function fadeInAnimation() {
+  const animationLayer = document.querySelector('.animation-layer');
+  const logo = document.querySelector(".logo-big");
+  setTimeout(() => {
+    document.querySelector(".login-container").style.opacity = "1";
+    document.querySelector(".sign-up-container").style.opacity = "1";
+  }, 1900);
 
+  setTimeout(function() {
+    animationLayer.style.display = 'none';
+    logo.src = "assets/icons/logo-black.png";
+  }, 1000);
+}
 
+function changeLogoColor() {
+  if (window.innerWidth < 800) {
+    const logo = document.querySelector(".logo-big");
+    logo.src = "assets/icons/logo-white-blue.png";
+    document.querySelector('.animation-layer').style.backgroundColor = '#2A3647';
+  } else {
+    document.querySelector('.animation-layer').style.backgroundColor = 'white';
+  }
+}
 
-function fadeInAnimation(){
-    setTimeout(() => {
-        document.querySelector('.login-container').style.opacity = '1';
-        document.querySelector('.sign-up-container').style.opacity = '1';
-      }, 1900);
+function changeArrowColor() {
+  if (window.innerWidth < 800) {
+    const arrow = document.getElementById("blueArrowLeft");
+    arrow.src = "assets/icons/arrow-left-black.png";
+  }
 }
