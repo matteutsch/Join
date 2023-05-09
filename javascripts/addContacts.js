fc48c77156e986ecdabbb5ad169f19590aac1f2c
matteutsch <matteutsch@gmail.com>
2023-05-09 16:47:26 +0200

    combined add-/editcontacts with contacts.html

---
+++ javascripts/addContacts.js
@@ -0,0 +1,17 @@
+function openNewContact() {
+  document.getElementById(`addContactsOverlay`).classList.remove("d-none");
+}
+
+function closeNewContact() {
+  document.getElementById(`addContactsOverlay`).classList.add("d-none");
+}
+
+function openEditContact() {
+  document.getElementById(`editContactsOverlay`).classList.remove("d-none");
+}
+
+function closeEditContact() {
+  document.getElementById(`editContactsOverlay`).classList.add("d-none");
+}
+
+
