f8970331d090bb07213df00c4a34aa2119eb6732
matteutsch <matteutsch@gmail.com>
2023-05-10 20:21:00 +0200

    responsive header/sidemenu/task/contacts to 850px

---
+++ javascripts/addTask.js
@@ -0,0 +1,24 @@
+function setPrio(prio) {
+  if (prio == "urgent") {
+    document.getElementById("urgentTask").classList.add("urgent");
+    document.getElementById("urgentIcon").src = "assets/icons/urgent_white.png";
+    document.getElementById("mediumTask").classList.remove("medium");
+    document.getElementById("mediumIcon").src = "assets/icons/medium.png";
+    document.getElementById("lowTask").classList.remove("low");
+    document.getElementById("lowIcon").src = "assets/icons/low.png";
+  } else if (prio == "medium") {
+    document.getElementById("mediumTask").classList.add("medium");
+    document.getElementById("mediumIcon").src = "assets/icons/urgent_white.png";
+    document.getElementById("urgentTask").classList.remove("urgent");
+    document.getElementById("urgentIcon").src = "assets/icons/urgent.png";
+    document.getElementById("lowTask").classList.remove("low");
+    document.getElementById("lowIcon").src = "assets/icons/low.png";
+  } else if (prio == "low") {
+    document.getElementById("lowTask").classList.add("low");
+    document.getElementById("lowIcon").src = "assets/icons/urgent_white.png";
+    document.getElementById("urgentTask").classList.remove("urgent");
+    document.getElementById("urgentIcon").src = "assets/icons/urgent.png";
+    document.getElementById("mediumTask").classList.remove("medium");
+    document.getElementById("mediumIcon").src = "assets/icons/medium.png";
+  }
+}
--- javascripts/storage.js
+++ javascripts/storage.js
@@ -1,66 +1,71 @@
-const STORAGE_TOKEN = 'HT0S0N13Y0K6B2YIWFIVXQ2L8P2T85JJ2LNGCLH0'
-const STORAGE_URL = 'https://remote-storage.developerakademie.org/item'
+const STORAGE_TOKEN = "HT0S0N13Y0K6B2YIWFIVXQ2L8P2T85JJ2LNGCLH0";
+const STORAGE_URL = "https://remote-storage.developerakademie.org/item";
 
-
-
-async function setItem(key, value){
-    const payload = { key, value, token: STORAGE_TOKEN};
-    return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload)})
-    .then(res => res.json())
+async function setItem(key, value) {
+  const payload = { key, value, token: STORAGE_TOKEN };
+  return fetch(STORAGE_URL, {
+    method: "POST",
+    body: JSON.stringify(payload),
+  }).then((res) => res.json());
 }
 
-async function getItem(key){
-    const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
-    return fetch(url).then(res => res.json()).then(res => res.data.value);
+async function getItem(key) {
+  const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
+  return fetch(url)
+    .then((res) => res.json())
+    .then((res) => res.data.value);
 }
 
 let contacts = [
-    {
-      name: "Anton Mayer",
-      initials: "AM",
-      email: "antom@gmail.com",
-    },
-    {
-      name: "Anja Schulz",
-      initials: "AS",
-      email: "schulz@hotmail",
-    },
-    {
-      name: "Benedikt Ziegler",
-      initials: "BZ",
-      email: "benedikt@gmail",
-    },
-    {
-      name: "David Eisenberg",
-      initials: "DE",
-      email: "davidberg@gmail",
-    },
-  ];
-  
-  let state = ["To Do", "In Progress", "Awaiting Feedback", "Done"];
-  let cat = ["Design", "Sales", "Backoffice", "Marketing", "Media"];
-  let prio = ["urgent", "medium", "low"];
-  
-  let tasks = [
-    {
-      title: "",
-      description: "",
-      status: state[0],
-      category: cat[1],
-      priority: prio[2],
-      subtasks: [],
-      dueDate: "",
-      assignedTo: [contacts[0], contacts[2], contacts[3]],
-    },
-    {
-      title: "",
-      description: "",
-      status: state[2],
-      category: cat[4],
-      priority: prio[0],
-      subtasks: [],
-      dueDate: "",
-      assignedTo: [contacts[2], contacts[3], contacts[4]],
-    },
-  ];
-  
+  {
+    name: "Anton Mayer",
+    initials: "AM",
+    email: "antom@gmail.com",
+    password: "xyz",
+  },
+  {
+    name: "Anja Schulz",
+    initials: "AS",
+    email: "schulz@hotmail",
+    password: "xyz",
+  },
+  {
+    name: "Benedikt Ziegler",
+    initials: "BZ",
+    email: "benedikt@gmail",
+    password: "xyz",
+  },
+  {
+    name: "David Eisenberg",
+    initials: "DE",
+    email: "davidberg@gmail",
+    password: "xyz",
+  },
+];
+
+let state = ["To Do", "In Progress", "Awaiting Feedback", "Done"];
+let cat = ["Design", "Sales", "Backoffice", "Marketing", "Media"];
+let prio = ["urgent", "medium", "low"];
+
+let tasks = [
+  {
+    title: "",
+    description: "",
+    status: state[0],
+    category: cat[1],
+    priority: prio[2],
+    subtasks: [],
+    dueDate: "",
+    assignedTo: [contacts[0], contacts[2], contacts[3]],
+  },
+  {
+    title: "",
+    description: "",
+    status: state[2],
+    category: cat[4],
+    priority: prio[0],
+    subtasks: [],
+    dueDate: "",
+    assignedTo: [contacts[2], contacts[3], contacts[4]],
+  },
+];
