177754ac76e5a1fb16bcc5cb36ef7f2dfb7cae3d
FurkanAyhan <109309647+FurkanAy97@users.noreply.github.com>
2023-05-11 16:22:00 +0200

    Progress update

--- javascripts/board.js
+++ javascripts/board.js
@@ -13,16 +13,18 @@ function renderCategoryLabelColor(i) {
 
 function renderTaskCards(container, status) {
   for (let i = 0; i < tasks.length; i++) {
+    let cardID = tasks[i]["status"] + i;
     const taskContainer = document.getElementById(container);
     const task = tasks[i];
     if (task["status"] === status) {
-      taskContainer.innerHTML += taskCardHTML(i);
+      taskContainer.innerHTML += taskCardHTML(i, cardID);
     }
   }
 }
 
-function taskCardHTML(i) {
-  return `<div class="task-card">
+function taskCardHTML(i, cardID) {
+  return `
+  <div class="task-card" id="${cardID}" onclick="openTaskCard(${i}, '${cardID}')">
     <div
       class="category-label"
       style="background-color: ${renderCategoryLabelColor(i)};">
@@ -44,6 +46,32 @@ function taskCardHTML(i) {
 function renderTaskDescription(i) {
   let description = tasks[i]["description"];
   return description;
+}
+
+function openTaskCard(i, cardID) {
+  console.log(cardID);
+  document.getElementById(cardID).innerHTML = openTaskCardHTML(i);
+}
+
+function openTaskCardHTML(i) {
+  return `<div class="task-card" id="${tasks[i]["status"]}${i}">
+  <div class="category-label" style="background-color: ${renderCategoryLabelColor(
+    i
+  )};">
+    ${tasks[i]["category"][0].toUpperCase() + tasks[i]["category"].slice(1)}
+    </div>
+  <div class="task-title">${tasks[i]["title"]}</div>
+  <div class="task-description">
+    ${renderTaskDescription(i)}
+  </div>
+  <div class="task-bottom">
+    <div class="assignedTo-container">DE</div>
+    <div class="task-urgency-container">
+      <img src="${renderUrgencyImg(i)}" />
+    </div>
+  </div>
+</div>
+  `;
 }
 
 function renderUrgencyImg(i) {
