4d6743f1cd8c191ea18ce45aa66b8bded9a1ff6d
matteutsch <matteutsch@gmail.com>
2023-05-13 13:19:00 +0200

    mobile responsive for contacts, sidemenu & header

--- javascripts/addTask.js
+++ javascripts/addTask.js
@@ -8,14 +8,14 @@ function setPrio(prio) {
     document.getElementById("lowIcon").src = "assets/icons/low.png";
   } else if (prio == "medium") {
     document.getElementById("mediumTask").classList.add("medium");
-    document.getElementById("mediumIcon").src = "assets/icons/urgent_white.png";
+    document.getElementById("mediumIcon").src = "assets/icons/medium_white.png";
     document.getElementById("urgentTask").classList.remove("urgent");
     document.getElementById("urgentIcon").src = "assets/icons/urgent.png";
     document.getElementById("lowTask").classList.remove("low");
     document.getElementById("lowIcon").src = "assets/icons/low.png";
   } else if (prio == "low") {
     document.getElementById("lowTask").classList.add("low");
-    document.getElementById("lowIcon").src = "assets/icons/urgent_white.png";
+    document.getElementById("lowIcon").src = "assets/icons/low_white.png";
     document.getElementById("urgentTask").classList.remove("urgent");
     document.getElementById("urgentIcon").src = "assets/icons/urgent.png";
     document.getElementById("mediumTask").classList.remove("medium");
