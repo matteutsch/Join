3e5e867dc8ba0eace5ae2c236b14ceaa44b22a12
User42-1 <jens.schrecker@t-online.de>
2023-05-15 08:42:57 +0200

    further improved summary js

--- javascripts/summary.js
+++ javascripts/summary.js
@@ -1,6 +1,7 @@
 function startSummary() {
     includeHTML()
     greet()
+    getDataForSummary()
 }
 /**
  * Changes greeting according to the time of day
@@ -17,4 +18,34 @@ function greet() {
       timeOfDay = "Good evening,";
     }
     document.getElementById("greetingText").innerHTML = timeOfDay;
+}
+
+/**
+ * Sumes up tasks per category and urgency per priority
+ */
+function getDataForSummary() {
+  document.getElementById('tasksInBoard').innerHTML = tasks.length;
+
+  let tasksInProgress = tasks.filter((task) => task.status === "todo");
+  document.getElementById('tasksInProgress').innerHTML = tasksInProgress.length;
+
+  let tasksAwaitingFeedback = tasks.filter((task) => task.status === "awaiting-feedback");
+  document.getElementById('tasksAwaitingFeedback').innerHTML = tasksAwaitingFeedback.length;
+
+  let sumToDo = tasks.filter((task) => task.status === "todo");
+  document.getElementById('sumToDo').innerHTML = sumToDo.length;
+
+  let sumDone = tasks.filter((task) => task.status === "done");
+  document.getElementById('sumDone').innerHTML = sumDone.length;
+
+  let sumUrgent = tasks.filter((task) => task.priority === "urgent");
+  document.getElementById('sumUrgent').innerHTML = sumUrgent.length;
+
+  let deadlines = tasks.filter((task) => task.dueDate === true);
+  console.log(deadlines);
+/*   document.getElementById('dateDeadline').innerHTML = sumUrgent.length;
+ */
+
+
+  
 }
