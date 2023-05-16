7fefc9cb5f889cbef2891a2ea3a8d9ef4f3e5819
matteutsch <matteutsch@gmail.com>
2023-05-16 19:33:46 +0200

    ..

--- javascripts/categoryColor.js
+++ javascripts/categoryColor.js
@@ -1,8 +1,21 @@
-const categoryColor = [
-    {
-        "sales": "#FC71FF",
-        "backoffice": "#1FD7C1",
-        "marketing": "#0038FF",
-        "design": "#FF7A00",
-    }
-]
+let categoryColor = [
+  {
+    sales: "#FC71FF",
+    backoffice: "#1FD7C1",
+    marketing: "#0038FF",
+    design: "#FF7A00",
+  },
+];
+
+let nameColor = [
+  "#0223CF",
+  "#CB02CF",
+  "#FFA800",
+  "#9327FF",
+  "#02CF2F",
+  "#EE00D6",
+  "#0190E0",
+  "#FF5C00",
+  "#4E963D",
+  "#32DAFF",
+];
